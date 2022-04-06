48e45cc330170c7c4c512a554a63efe88715eaf1
samura <tp6m4xup6tp6@gmail.com>
2022-04-06 16:14:06 +0800

    統整 if 邏輯

--- js-mocha/src/gilded_rose.js
+++ js-mocha/src/gilded_rose.js
@@ -43,9 +43,7 @@ class Shop {
         if (this.items[i].sellIn < 0 && this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1;
         }
-      }
-
-      if(this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+      } else if(this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') { 
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].sellIn < 11) {
@@ -59,37 +57,21 @@ class Shop {
             }
           }
         }
-
+  
         this.items[i].sellIn = this.items[i].sellIn - 1;
         if (this.items[i].sellIn < 0) { 
           this.items[i].quality = this.items[i].quality - this.items[i].quality;
         }
-      }
-
-      if(this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-
-
+      } else if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
+          this.items[i].quality = this.items[i].quality - 1;
         }
   
-  
-        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          this.items[i].sellIn = this.items[i].sellIn - 1;
-        }
-  
-  
-        if (this.items[i].sellIn < 0) {
-          if (this.items[i].quality > 0) {
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              this.items[i].quality = this.items[i].quality - 1;
-            }
-          }
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+        if (this.items[i].sellIn < 0 && this.items[i].quality > 0) {
+          this.items[i].quality = this.items[i].quality - 1;
         }
       }
-      
     }
 
     return this.items;
